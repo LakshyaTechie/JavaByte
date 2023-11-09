e6a655ff170bf6d0dd28c9edfcd8bf383462a362
LakshyaTechie <150327846+LakshyaTechie@users.noreply.github.com>
2023-11-09 13:14:39 +0530

    Create ArrayList.java
    first commit

---
+++ ArrayList.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class ArrayList {
+
+	public static void main(String[] args) {
+		
+		List<Integer> ls= Arrays.asList(1,2,3,4,5);
+		
+		System.out.println(ls);
+	}
+
+}
