1e4e2aa5886d8da7d56b540aba2569de6a22fe9c
BaluLakshya <codetobyte@gmail.com>
2023-11-09 14:09:44 +0530

    commit to Dev

---
+++ com/nihaal/dev/TestA.java
@@ -0,0 +1,12 @@
+package com.nihaal.dev;
+
+public class TestA {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello Test Class");
+
+	}
+
+}
